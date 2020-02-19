9dca630f913b8886cc4a1639b033b8b7c8376f3a
William Sedlacek <wsedlacekc@gmail.com>
2020-02-19 17:31:14 -0600

    Rock Paper Scissors
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,10 +1,37 @@
 #!/usr/bin/python
 
 import sys
+from typing import Any, List
 
 
-def rock_paper_scissors(n):
-    pass
+# For each player
+# there are three options
+# for every option of all other players
+
+def multiply_list(list1: List[Any], list2: List[Any]):
+    if not list1:
+        return list2
+
+    if not list2:
+        return list1
+
+    combined = []
+    for item1 in list1:
+        for item2 in list2:
+            combined.append([*item1, *item2])
+
+    return combined
+
+
+def rock_paper_scissors(player_count):
+    if player_count <= 0:
+        return [[]]
+
+    options = [["rock"], ["paper"], ["scissors"]]
+    previous_combinations = rock_paper_scissors(player_count - 1)
+    combinations = multiply_list(options, previous_combinations)
+
+    return combinations
 
 
 if __name__ == "__main__":
